275eea14e2a1056a31d01a0dfc6abaf79d045aba
Christine Colvin <christinegracecolvin@gmail.com>
2026-02-12 07:39:16 -0500

    add business/internal logic with search variable that takes in strings and returns lists

---
+++ internalLogic.py
@@ -0,0 +1,9 @@
+class internalLogic():
+    def init(self, database):
+        self.database = database
+        pass
+
+    def search(self, query):
+        # Placeholder for processing logic
+        processed_data = [self.database + " processed with query: " + query]
+        return processed_data
--- src/mirrsearch/app.py
+++ src/mirrsearch/app.py
@@ -1,17 +1,22 @@
 from flask import Flask
 
+import internalLogic
+
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(name)
 
     @app.route("/")
     def hello_world():
         return "<p>Hello, World!</p>"
 
+    @app.route("/search")
+    def search():
+        return internalLogic("sample_database").search("example_query")
+
     return app
 
 
-if __name__ == '__main__':
+if name == 'main':
     app = create_app()
     app.run(port=8000, debug=True)
-
