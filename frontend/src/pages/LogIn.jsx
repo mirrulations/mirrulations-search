66863a186f51a4721079cbd8b62ff1fa63b7ef4b
Francisco <figueroaf@moravian.edu>
2026-02-28 22:37:35 -0500

    Added some basic styling

--- frontend/src/pages/LogIn.jsx
+++ frontend/src/pages/LogIn.jsx
@@ -1,19 +1,36 @@
 import React from 'react'
 import '../styles/LogIn.css'
+import { motion } from "motion/react"
 
 const LogIn = () => {
-  return (
-    <div className="login-wrapper">
-      <div className="login-box">
-        <h2>Log In</h2>
-
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-
-        <button>Log In</button>
-      </div>
-    </div>
-  )
+    return (
+        <motion.div className="login-wrapper"
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.3, duration: 1.5, ease: "easeInOut" }}
+        >
+            <div className="login-box floating-effect">
+                <motion.h2
+                 initial={{ opacity: 0, y: -20 }}
+                 animate={{ opacity: 1, y: 0 }}
+                 transition={{ delay: 0.7, duration: 1, ease: "easeInOut" }}
+                >LOG IN</motion.h2>
+                <div className='inputs-div'>
+                    <motion.input type="email" placeholder="Email"
+                     initial={{ opacity: 0, y: -20 }}
+                     animate={{ opacity: 1, y: 0 }}
+                     transition={{ delay: 1.2, duration: 1, ease: "easeInOut" }}
+                    />
+                    <motion.input type="password" placeholder="Password" 
+                     initial={{ opacity: 0, y: -20 }}
+                     animate={{ opacity: 1, y: 0 }}
+                     transition={{ delay: 1.6, duration: 1, ease: "easeInOut" }}
+                    />
+                    <button>LOG IN</button>
+                </div>
+            </div>
+        </motion.div>
+    )
 }
 
 export default LogIn
