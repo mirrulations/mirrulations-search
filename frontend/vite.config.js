dc9ccd8c466d0c0bc927d4bbdf14a986ca79a4fa
Francisco <figueroaf@moravian.edu>
2026-04-06 21:20:00 -0400

    Remove unused dependencies (npm, i) and turn off sourcemaps and compressed size reporting to make builds faster.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
+    build: {
+        // Skips gzip-size step; which should save more time on small/CI/EC2 stuff
+        reportCompressedSize: false,
+        sourcemap: false,
+    },
     server: {
         proxy: {
             '/search': 'http://127.0.0.1:80',
